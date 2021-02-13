94f5baac09233f8bda38b91621fb8d1bdad54b51
Dusan Jovanov <jovanovdusan1@gmail.com>
2021-02-13 17:55:37 +0100

    asdasd

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -5,13 +5,25 @@ import { ResolverMap } from "../../types/graphql-utils";
 export const resolvers: ResolverMap = {
   Mutation: {
     register: async (_, { email, password }) => {
+      const userAlreadyExists = User.findOne({
+        where: { email },
+        select: ["id"],
+      });
+      if (userAlreadyExists) {
+        return [
+          {
+            path: "email",
+            message: "Email taken",
+          },
+        ];
+      }
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = await User.create({
         email,
         password: hashedPassword,
       });
       await user.save();
-      return true;
+      return null;
     },
   },
 };
