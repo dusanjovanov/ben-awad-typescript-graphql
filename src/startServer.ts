fcb8949b86fc9221680ec538dc97dd42e79b68f5
Dusan Jovanov <jovanovdusan1@gmail.com>
2021-02-13 14:42:36 +0100

    merge schemas, modular structure

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,12 +1,22 @@
 import { importSchema } from "graphql-import";
 import { GraphQLServer } from "graphql-yoga";
 import path from "path";
-import { resolvers } from "./resolvers";
 import { createTypeORMConnection } from "./utils/createTypeORMConnection";
-const typeDefs = importSchema(path.resolve("./src/schema.graphql"));
+import fs from "fs";
+import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
+import { GraphQLSchema } from "graphql";
 
 export const startServer = async () => {
-  const server = new GraphQLServer({ typeDefs, resolvers });
+  const schemas: GraphQLSchema[] = [];
+  const folders = fs.readdirSync(path.join(__dirname, "modules"));
+  folders.forEach(folder => {
+    const { resolvers } = require(`./modules/${folder}/resolvers`);
+    const typeDefs = importSchema(
+      path.join(__dirname, `./modules/${folder}/schema.graphql`)
+    );
+    schemas.push(makeExecutableSchema({ typeDefs, resolvers }));
+  });
+  const server = new GraphQLServer({ schema: mergeSchemas({ schemas }) });
   await createTypeORMConnection();
   const app = await server.start({
     port: process.env.NODE_ENV === "test" ? 0 : 4000,
